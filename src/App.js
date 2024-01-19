b7d7a728b4a85b23ddcf1c0d69fd92a48ca78d99
gmoll <gmollq@gmail.com>
2024-01-19 15:20:25 +0300

    Add task code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Field from './Field.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Field speed={100} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Field.js
@@ -0,0 +1,12 @@
+// Speed param measured in degrees/second
+
+function Field({ speed }) {
+  return (
+    <div className="Field">
+      <div className="Square" />
+      <div className="Point" />
+    </div>
+  );
+}
+
+export default Field;
